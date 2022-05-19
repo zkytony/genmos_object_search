5df9e1bc4dddc28c386f267dc0d52da85602a977
Kaiyu Zheng <kaiyutony@gmail.com>
2022-05-19 14:09:52 -0400

    sloop_planning listens to sloop belief

--- scripts/sloop_planning.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from sloop_ros.sloop_planning import sloop_planner
-
-if __name__ == "__main__":
-    try:
-        sloop_planner()
-    except rospy.ROSInterruptException:
-        pass
--- src/sloop_ros/sloop_planning.py
+++ src/sloop_ros/sloop_planning.py
@@ -1,13 +1,23 @@
-# license removed for brevity
+# The sloop_planner is responsible for:
+#
+# (1) creating the POMDP Agent for the SLOOP task
+# (2) listening to belief and then use that to produce the next action
+# (3) publish the next action
 import rospy
 from std_msgs.msg import String
 
+def belief_callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    sloop_plan_next()
+
+def sloop_plan_next():
+    print("Planning next action")
+
 def sloop_planner():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world sloop_planner %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+    rospy.init_node('sloop_planner', anonymous=True)
+    agent = "agent"
+
+    sloop_belief_topic = rospy.get_param("~belief_topic")
+    belief_sub = rospy.Subscriber(sloop_belief_topic, String, belief_callback)
+
+    rospy.spin()
