c7009f46805f9c9f68759be52e5a7f516070d58d
Kaiyu Zheng <kaiyutony@gmail.com>
2022-05-29 15:51:21 -0400

    start over

--- setup.py
+++ setup.py
@@ -1,34 +1,11 @@
 # ## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+from setuptools import setup
+from catkin_pkg.python_setup import generate_distutils_setup
 
-IN_ROS_WS = False
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    packages=['sloop_ros'],
+    package_dir={'': 'src'}
+)
 
-if IN_ROS_WS:
-    from setuptools import setup
-    from catkin_pkg.python_setup import generate_distutils_setup
-
-    # fetch values from package.xml
-    setup_args = generate_distutils_setup(
-        packages=['sloop_ros', 'sloop'],
-        package_dir={'': 'src'}
-    )
-
-    setup(**setup_args)
-
-else:
-    from setuptools import setup
-
-    setup(name='sloop-ros',
-          packages=['sloop_ros', 'sloop'],
-          package_dir={'': 'src'},
-          version='0.0',
-          description='SLOOP ROS',
-          python_requires='>3.6',
-          install_requires=[
-              'pyyaml',
-              'numpy',
-              'pomdp-py',
-              'pandas'
-          ],
-          author='Kaiyu Zheng',
-          author_email='kaiyutony@gmail.com'
-         )
+setup(**setup_args)
--- src/sloop/README.md
+++
@@ -1,1 +0,0 @@
-This package implements SLOOP, while integrating key parts of COS-POMDP, including the hierarchical planning algorithm.
