c098abda20e53eb37f6ea0c54b1e22f78b389c2c
Kaiyu Zheng <kaiyutony@gmail.com>
2022-05-31 20:49:27 -0400

    bug fix in install_dependencies

--- install_dependencies.bash
+++ install_dependencies.bash
@@ -1,12 +1,12 @@
 set -o xtrace
-pip install numpy>=1.20.3
-pip install opencv-contrib-python>=4.5.1.48
-pip install opencv-python>=4.1.1.26
-pip install pandas>=1.1.2
-pip install Pillow>=8.2.0
-pip install pygame>=1.9.6
-pip install scipy>=1.3.3
-pip install seaborn>=0.11.0
+pip install 'numpy>=1.20.3'
+pip install 'opencv-contrib-python>=4.5.1.48'
+pip install 'opencv-python>=4.1.1.26'
+pip install 'pandas>=1.1.2'
+pip install 'Pillow>=8.2.0'
+pip install 'pygame>=1.9.6'
+pip install 'scipy>=1.3.3'
+pip install 'seaborn>=0.11.0'
 # Note that we are trying out versions of the following packages
 pip install pomdp-py
 pip install nltk   # tested 3.7
