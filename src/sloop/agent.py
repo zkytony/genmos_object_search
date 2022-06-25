fade7e6425638d4964c9b62467cc0ea0036dbbe3
Kaiyu Zheng <kaiyutony@gmail.com>
2022-06-25 00:04:22 -0400

    force load honolulu model for now

--- src/sloop/agent.py
+++ src/sloop/agent.py
@@ -28,7 +28,8 @@ class SloopAgent(pomdp_py.Agent):
             self._init_splang_observation_model(
                 agent_config["foref_models_dir"],
                 device=agent_config.get("device", "cuda:0"),
-                symbol_map=agent_config.get("object_symbol_map", None))
+                symbol_map=agent_config.get("object_symbol_map", None),
+                foref_model_map_name=agent_config.get("foref_model_map_name", map_name))  # which foref model to load
 
         pomdp_components = self._init_oopomdp(**pomdp_kwargs)
         super().__init__(*pomdp_components)
@@ -42,14 +43,19 @@ class SloopAgent(pomdp_py.Agent):
     def _init_splang_observation_model(self,
                                        foref_models_dir,
                                        device="cpu",
-                                       symbol_map=None):
+                                       symbol_map=None,
+                                       foref_model_map_name=None):
         def _mp(predicate):
             """model path"""
             iteration = 2
+            if foref_model_map_name is not None:
+                map_name = foref_model_map_name
+            else:
+                map_name = self.map_name
             return os.path.join(foref_models_dir,
                                 "iter%s_%s:%s:%s"
                                 % (iteration, "ego-ctx-foref-angle",
-                                   predicate, self.map_name),
+                                   predicate, map_name),
                                 "%s_model.pt" % predicate)
 
         device = torch.device(device if torch.cuda.is_available() else "cpu")
