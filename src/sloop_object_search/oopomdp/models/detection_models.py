a91e41fa865de92d368cd3734a43bc7a46154b2b
Kaiyu Zheng <kaiyutony@gmail.com>
2022-07-14 12:46:17 -0400

    no need to check zi.loc for LabelOnly detector

--- src/sloop_object_search/oopomdp/models/detection_models.py
+++ src/sloop_object_search/oopomdp/models/detection_models.py
@@ -482,11 +482,6 @@ class FanModelSimpleFPLabelOnly(FanModel):
                 # false negative
                 return 1.0 - self.detection_prob
             else:
-                if not srobot.loc_in_range(self.sensor, zi.loc):
-                    # the robot would not have received such a positive observation,
-                    # because it is outside of the FOV. It is treatd as a false positive,
-                    # that comes uniformly likely outside of the FOV.
-                    return self.false_pos_rate
                 # true positive
                 return self.detection_prob
         else:
