4979fc86b3e5102d5592d32d2b17f93294533b76
Kaiyu Zheng <kaiyutony@gmail.com>
2022-07-20 14:13:14 -0400

    path fix to arm_grasp example

--- scripts/spot_automate_task_and_grasp.sh
+++ scripts/spot_automate_task_and_grasp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 roslaunch sloop_ros sloop_mos_spot_automate_task.launch max_steps:=50
-cd ~/repo/robotdev/spot/spot-sdk/python/examples
+cd ~/repo/robotdev/spot/spot-sdk/python/examples/arm_grasp
 python arm_grasp.py -i hand_color_image $SPOT_IP
