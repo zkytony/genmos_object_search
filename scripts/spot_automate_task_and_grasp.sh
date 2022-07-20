dc72873abf1338f366bd9b0ec2d5c8a51f5626a0
Kaiyu Zheng <kaiyutony@gmail.com>
2022-07-20 14:42:32 -0400

    path fix in arm grasp.sh

--- scripts/spot_automate_task_and_grasp.sh
+++ scripts/spot_automate_task_and_grasp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 roslaunch sloop_ros sloop_mos_spot_automate_task.launch max_steps:=50
-roscd sloop_ros/scripts
+cd ~/repo/robotdev/spot/ros_ws/src/sloop_ros/scripts
 python spot_arm_grasp.py -i hand_color_image $SPOT_IP
