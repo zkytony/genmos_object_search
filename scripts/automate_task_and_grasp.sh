d4cb093bcb21ab0d23c91651064d48dd7bf79aa5
Kaiyu Zheng <kaiyutony@gmail.com>
2022-07-20 13:39:25 -0400

    automate task shell script

--- scripts/automate_task_and_grasp.sh
+++
@@ -1,3 +0,0 @@
-roslaunch sloop_ros sloop_mos_spot_automate_task.launch
-cd ~/repo/robotdev/spot/spot-sdk/python/examples
-python arm_grasp.py -i hand_color_image $SPOT_IP
---
+++ scripts/spot_automate_task_and_grasp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+roslaunch sloop_ros sloop_mos_spot_automate_task.launch max_steps:=50
+cd ~/repo/robotdev/spot/spot-sdk/python/examples
+python arm_grasp.py -i hand_color_image $SPOT_IP
