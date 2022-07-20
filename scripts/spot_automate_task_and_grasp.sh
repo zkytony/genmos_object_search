014e20f00044a0484fcbfabb33eff2cd3c178e40
Kaiyu Zheng <kaiyutony@gmail.com>
2022-07-20 14:38:38 -0400

    copying & modifying arm_grasp example and use it

--- scripts/spot_automate_task_and_grasp.sh
+++ scripts/spot_automate_task_and_grasp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 roslaunch sloop_ros sloop_mos_spot_automate_task.launch max_steps:=50
-cd ~/repo/robotdev/spot/spot-sdk/python/examples/arm_grasp
-python arm_grasp.py -i hand_color_image $SPOT_IP
+roscd sloop_ros/scripts
+python spot_arm_grasp.py -i hand_color_image $SPOT_IP
