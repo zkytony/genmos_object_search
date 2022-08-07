9884b5436d3f6bde7cd56f7dc149b49fada4151d
Kaiyu Zheng <kaiyutony@gmail.com>
2022-08-07 09:52:46 -0400

    state of passing tests

--- src/sloop_object_search/oopomdp/models/__init__.py
+++ src/sloop_object_search/oopomdp/models/__init__.py
@@ -1,3 +1,3 @@
 """**Defines models, including transition, observation, reward, policy; Also
 includes additional components such as the sensor model and grid map.**"""
-from .octree_belief import octree_belief, octree
+# from .octree_belief import octree_belief, octree
