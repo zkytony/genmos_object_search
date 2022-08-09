d2717ee508d75165919f664751780b7d486e51e0
Kaiyu Zheng <kaiyutony@gmail.com>
2022-08-09 15:02:24 -0400

    approximate time sync as class member

--- include/sloop_object_search/ros/processing/local_cloud_publisher.h
+++ include/sloop_object_search/ros/processing/local_cloud_publisher.h
@@ -1,19 +1,43 @@
 #ifndef LOCAL_CLOUD_PUBLISHER_H
 #define LOCAL_CLOUD_PUBLISHER_H
 
+#include <string>
+
 #include <ros/ros.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+#include <sensor_msgs/PointCloud2.h>
+#include <geometry_msgs/PoseStamped.h>
+
+using std::string;
+using sensor_msgs::PointCloud2;
+using geometry_msgs::PoseStamped;
+
+using namespace message_filters;
+using namespace message_filters::sync_policies;
+
+typedef ApproximateTime<PointCloud2, PoseStamped> CloudPoseSyncPolicy;
+typedef Synchronizer<CloudPoseSyncPolicy> CloudPoseSync;
 
 class LocalCloudPublisher {
 public:
     LocalCloudPublisher();
+
     void run();
+
+    void cloudPoseCallback(const PointCloud2 &cloud, const PoseStamped &pose);
+
+    ~LocalCloudPublisher();
+
 private:
     ros::NodeHandle nh_;
-    ros::Subscriber pcl_global_sub_;
     ros::Publisher pcl_local_pub_;
+    message_filters::Subscriber<PointCloud2> *pcl_global_sub_;
+    message_filters::Subscriber<PoseStamped> *robot_pose_sub_;
+    CloudPoseSync *sync_;
     double pub_rate_;
-
 };
-
 
 #endif
--- src/sloop_object_search/ros/processing/local_cloud_publisher.cpp
+++ src/sloop_object_search/ros/processing/local_cloud_publisher.cpp
@@ -3,43 +3,50 @@
 
 #include <ros/ros.h>
 #include <message_filters/subscriber.h>
-#include <message_filters/synchronizer.h>
-#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/time_synchronizer.h>
 
 #include <sensor_msgs/PointCloud2.h>
-#include <geometry_msgs/PoseStamped.h>
+
+#include "local_cloud_publisher.h"
 
 using std::string;
 using sensor_msgs::PointCloud2;
 using geometry_msgs::PoseStamped;
 
-using namespace message_filters;
-using namespace message_filters::sync_policies;
-
-typedef Synchronizer<ApproximateTime<PointCloud2, PoseStamped>> CloudPoseSync;
-
-void cloudPoseCallback(const PointCloud2 &cloud, const PoseStamped &pose_stamped) {
-
-    double x = pose_stamped.pose.position.x;
-    double y = pose_stamped.pose.position.y;
-    double z = pose_stamped.pose.position.z;
-
+void LocalCloudPublisher::cloudPoseCallback(const PointCloud2 &cloud, const PoseStamped &pose) {
     std::cout << "RECEIVED MESSAGE!" << std::endl;
+}
+
+LocalCloudPublisher::LocalCloudPublisher()
+    : nh_(ros::NodeHandle("~")) {
+
+    pub_rate_ = nh_.param<double>("local_cloud_publish_rate", 4.0);
+    string global_cloud_topic = nh_.param<string>("global_cloud_topic", "global_points");
+    string robot_pose_topic = nh_.param<string>("robot_pose_topic", "robot_pose");
+
+    pcl_global_sub_ = new message_filters::Subscriber<PointCloud2>(nh_, global_cloud_topic, 10);
+    robot_pose_sub_ = new message_filters::Subscriber<PoseStamped>(nh_, robot_pose_topic, 10);
+
+    sync_ = new CloudPoseSync(CloudPoseSyncPolicy(10), *pcl_global_sub_, *robot_pose_sub_);
+    sync_->registerCallback(&LocalCloudPublisher::cloudPoseCallback, this);
+}
+
+void LocalCloudPublisher::run() {
+    ros::Rate loop_rate(this->pub_rate_);
+    while (this->nh_.ok()) {
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+}
+
+LocalCloudPublisher::~LocalCloudPublisher() {
+    delete pcl_global_sub_;
+    delete robot_pose_sub_;
+    delete sync_;
 }
 
 int main(int argc, char *argv[]) {
     ros::init(argc, argv, "local_cloud_publisher");
-
-    ros::NodeHandle nh(ros::NodeHandle("~"));
-
-    string global_cloud_topic = nh.param<string>("global_cloud_topic", "global_points");
-    string robot_pose_topic = nh.param<string>("robot_pose_topic", "robot_pose");
-
-    message_filters::Subscriber<PointCloud2> pcl_global_sub(nh, global_cloud_topic, 10);
-    message_filters::Subscriber<PoseStamped> robot_pose_sub(nh, robot_pose_topic, 10);
-
-    CloudPoseSync sync(10, pcl_global_sub, robot_pose_sub);
-    sync.registerCallback(cloudPoseCallback);
-
-    ros::spin();
+    LocalCloudPublisher locPub = LocalCloudPublisher();
+    locPub.run();
 }
