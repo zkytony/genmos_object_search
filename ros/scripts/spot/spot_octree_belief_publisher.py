039da4fc498bf7d18debf1242ab5143dc06baf07
Kaiyu Zheng <kaiyutony@gmail.com>
2022-08-10 17:46:14 -0400

    able to create octree belief from point cloud

--- ros/scripts/spot/spot_octree_belief_publisher.py
+++ ros/scripts/spot/spot_octree_belief_publisher.py
@@ -2,18 +2,41 @@
 #
 # Generates octree belief from given point cloud
 import argparse
+import numpy as np
 import rospy
 import ros_numpy
 from sensor_msgs.msg import PointCloud2
+from sloop_object_search.oopomdp.models.octree_belief import Octree, OctreeBelief
+from sloop_mos_ros.conversion import convert, Frame
+from sloop_object_search.oopomdp import ObjectState
+
+OBJID = "obj"
+
+VAL = 100
 
 class SpotOctreeBeliefPublisher:
     def __init__(self, args):
         point_cloud_topic = args.point_cloud_topic
         self._cloud_sub = rospy.Subscriber(point_cloud_topic, PointCloud2, self._cloud_cb)
+        octree = Octree(OBJID, (args.size, args.size, args.size))
+        self._octree_belief = OctreeBelief(args.size, args.size, args.size,
+                                           OBJID, "OBJ", octree)
+        self._search_space_res = args.res
 
-    def _cloud_cb(self, msg):
-        points = ros_numpy.point_cloud2.pointcloud2_to_array(msg)
-        print(len(points))
+
+    def _cloud_cb(self, pcl2msg):
+        pc = ros_numpy.numpify(pcl2msg)
+        points = np.zeros((pc.shape[0], 3))
+        points[:, 0] = pc['x']
+        points[:, 1] = pc['y']
+        points[:, 2] = pc['z']
+        origin = np.min(points, axis=0)
+        for p in points:
+            pomdp_point = convert(p, Frame.WORLD, Frame.POMDP_SPACE,
+                                  region_origin=origin,
+                                  search_space_resolution=self._search_space_res)
+            self._octree_belief[ObjectState(OBJID, "OBJ", pomdp_point, res=1)] = VAL
+        print("points incorporated to octree belief")
 
     def run(self):
         rospy.spin()
@@ -23,6 +46,10 @@ def main():
     rospy.init_node("spot_octree_belief_publisher")
     parser = argparse.ArgumentParser("Octree belief publisher from point cloud")
     parser.add_argument("point_cloud_topic", type=str, help="input point cloud to convert")
+    parser.add_argument("--size", type=int, help="size of one dimension of the space that the octree covers."
+                        "Must be a power of two. Recommended: 16, 32, or 64.", required=True)
+    parser.add_argument("--res", type=float, help="meter of a side of a ground-level cube in the octree.",
+                        required=True)
     parser.add_argument("--viz-topic", type=str, help="topic to publish visualization of the octree belief",
                         default="octree_belief_visual")
     args, _ = parser.parse_known_args()
---
+++ ros/src/sloop_mos_ros/conversion.py
@@ -0,0 +1,107 @@
+# Copyright 2022 Kaiyu Zheng
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Coordinate conversion
+#   gmapping coordinate is at lower-left. According to https://www.ros.org/reps/rep-0103.html,
+# the X axis is East, Y axis is North.
+#   Note that a point in the gmapping coordinate frame has unit in meter.
+import math
+
+class Frame:
+    # gmapping map
+    WORLD = "world"
+    # a rectangular region in the gmapping map
+    REGION = "region"
+    # the POMDP search space.
+    SEARCH_SPACE = "search_space"
+    POMDP_SPACE = "search_space"        # .. alias
+    POMDP_SEARCH_SPACE = "search_space" # .. alias
+
+def convert(point, from_frame, to_frame,
+            region_origin=None,
+            search_space_resolution=None):
+    """
+    `from_frame` (str) one of {"world", "region", or "search_space"}
+    `to_frame` (str) one of {"world", "region", or "search_space"}
+    """
+    all_frames = {Frame.WORLD, Frame.REGION, Frame.POMDP_SPACE}
+    if not (from_frame.lower() in all_frames and to_frame.lower() in all_frames):
+        raise ValueError("Unrecognized frame %s or %s" % (from_frame, to_frame))
+    if from_frame == to_frame:
+        return point
+
+    if from_frame == Frame.WORLD:
+        if to_frame == Frame.REGION:
+            assert region_origin is not None,\
+                "Converting from world to region requires origin of the region."
+            return _world2region(point, region_origin)
+        else:  # to_frame == search_space; convert twice
+            region_point = convert(point, Frame.WORLD, Frame.REGION,
+                                   region_origin=region_origin)
+            return convert(region_point, Frame.REGION, Frame.POMDP_SPACE,
+                           search_space_resolution=search_space_resolution)
+
+
+    elif from_frame == Frame.REGION:
+        if to_frame == Frame.POMDP_SPACE:
+            assert search_space_resolution is not None,\
+                "Converting from region to search_space requires resolution of the search space."
+            return _region2searchspace(point, search_space_resolution)
+        else: # to_frame == Frame.WORLD:
+            assert region_origin is not None,\
+                "Converting from region to world requires origin of the region."
+            return _region2world(point, region_origin)
+
+    elif from_frame == Frame.POMDP_SPACE:
+        if to_frame == Frame.REGION:
+            assert search_space_resolution is not None,\
+                "Converting from search_space to region requires resolution of the search space."
+            return _searchspace2region(point, search_space_resolution)
+        else: # to_frame == Frame.WORLD; convert twice
+            region_point = convert(point, Frame.SEARCH_SPACE, Frame.REGION,
+                                   search_space_resolution=search_space_resolution)
+            return convert(region_point, Frame.REGION, Frame.WORLD,
+                           region_origin=region_origin)
+
+
+def _world2region(world_point, region_origin):
+    """
+    Given `world_point` (x,y,z) in world coordinate (i.e. full gmapping map coordinate),
+    and the origin of the rectangular region, also in world coordinate,
+    returns the world point in region's coordinate frame. The region's
+    points will have the same resolution as the gmapping map.
+    """
+    # simply subtract world point x,y by region origin. Keep z unchanged.
+    return (world_point[0] - region_origin[0],
+            world_point[1] - region_origin[1],
+            world_point[2])
+
+def _region2world(region_point, region_origin):
+    """region_point(x,y,z) -> world_point(x,y,z)"""
+    return (region_point[0] + region_origin[0],
+            region_point[1] + region_origin[1],
+            region_point[2])
+
+def _region2searchspace(region_point, search_space_resolution):
+    """Convert region point to a cube's coordinate in the search space.
+    Assume that the search space's origin is at the region coordinate frame's
+    origin. The `search_space_resolution` has unit m/cell."""
+    return (int(math.floor(region_point[0] / search_space_resolution)),
+            int(math.floor(region_point[1] / search_space_resolution)),
+            int(math.floor(region_point[2] / search_space_resolution)))
+
+def _searchspace2region(search_space_point, search_space_resolution):
+    return (search_space_point[0] * search_space_resolution,
+            search_space_point[1] * search_space_resolution,
+            search_space_point[2] * search_space_resolution)
