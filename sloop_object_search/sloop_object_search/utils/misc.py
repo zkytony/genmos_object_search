5b2a1fdb1de33f0f6d8a974f7583052c9c48f877
Kaiyu Zheng <kaiyutony@gmail.com>
2022-08-10 09:48:30 -0400

    wrote test for octree belief performance

--- sloop_object_search/sloop_object_search/utils/misc.py
+++ sloop_object_search/sloop_object_search/utils/misc.py
@@ -1,5 +1,6 @@
 import importlib
 import hashlib
+import signal
 
 def import_class(class_string):
     """Returns the class pointed to by 'class_string',
@@ -23,3 +24,17 @@ def det_dict_hash(dct, keep=9):
     content = str(list(sorted(dct.items()))).encode()
     hashcode = int(str(int(hashlib.sha1(content).hexdigest(), 16))[:keep])
     return hashcode
+
+
+class timeout:
+    # https://stackoverflow.com/a/22348885/2893053
+    def __init__(self, seconds=1, error_message='Timeout'):
+        self.seconds = seconds
+        self.error_message = error_message
+    def handle_timeout(self, signum, frame):
+        raise TimeoutError(self.error_message)
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
---
+++ sloop_object_search/tests/sloop_object_search/perf/test_octree_belief_python_performance.py
@@ -0,0 +1,96 @@
+import time
+import random
+from sloop_object_search.oopomdp import ObjectState
+from sloop_object_search.oopomdp.models.octree_belief import Octree, OctreeBelief
+from sloop_object_search.utils.misc import timeout
+
+
+def test_octree_belief_performance_small():
+    print("\ntest_octree_belief_performance_SMALL")
+    with timeout(45):
+        _test_octree_belief_performance((16, 16, 16))
+
+def test_octree_belief_performance_normal():
+    print("\ntest_octree_belief_performance_NORMAL")
+    with timeout(45):
+        _test_octree_belief_performance((32, 32, 32))
+
+def test_octree_belief_performance_medium():
+    print("\ntest_octree_belief_performance_MEDIUM")
+    with timeout(45):
+        _test_octree_belief_performance((64, 64, 64))
+
+def test_octree_belief_performance_big():
+    print("\ntest_octree_belief_performance_BIG")
+    with timeout(45):
+        _test_octree_belief_performance((128, 128, 128))
+
+def test_octree_belief_performance_large():
+    print("\ntest_octree_belief_performance_LARGE")
+    with timeout(45):
+        _test_octree_belief_performance((256, 256, 256))
+
+def test_octree_belief_performance_huge():
+    print("\ntest_octree_belief_performance_HUGE")
+    with timeout(45):
+        _test_octree_belief_performance((1024, 1024, 1024))
+
+
+def _test_octree_belief_performance(dim,
+                                    sample_count=2000):
+    octree = Octree(1, dim)
+    octree_belief = OctreeBelief(*dim, 1, "cube", octree)
+
+    # Sampling from the tree - initial state
+    print("\n------ sampling from initial tree ------")
+    _start_time = time.time()
+    for i in range(sample_count):
+        s = octree_belief.random()
+    _total_time = time.time() - _start_time
+    num_leaves = len(octree_belief.octree.get_leaves())
+    print(f"Sampling {dim} octree belief (with {num_leaves} leaves) {sample_count} took {_total_time:.9f}s) ({_total_time/sample_count:.9f}s per sample)\n")
+
+    # Sampling from partial tree
+    print("------ sampling from partial tree ------")
+    num_samples = int(dim[0]*dim[1]*dim[2]*0.5)
+    positions = random.sample({(x,y,z)   # random sample without replacement
+                               for x in range(dim[0])
+                               for y in range(dim[1])
+                               for z in range(dim[2])}, num_samples)
+    _start_time = time.time()
+    for p in positions:
+        si = ObjectState(1, "cube", p, res=1)
+        octree_belief[si] = random.uniform(0,1)*10
+    _total_time = time.time() - _start_time
+    print(f"Inserting {num_samples} leaves to octree belief took {_total_time:.4f}s)")
+
+    _start_time = time.time()
+    for i in range(sample_count):
+        s = octree_belief.random()
+    _total_time = time.time() - _start_time
+    num_leaves = len(octree_belief.octree.get_leaves())
+    print(f"Sampling {dim} octree belief (with {num_leaves} leaves) {sample_count} took {_total_time:.4f}s) ({_total_time/sample_count:.9f}s per sample)\n")
+
+    # Sampling from partial tree
+    print("------ sampling from full tree ------")
+    # reset the tree.
+    octree = Octree(1, dim)
+    octree_belief = OctreeBelief(*dim, 1, "cube", octree)
+    num_samples = int(dim[0]*dim[1]*dim[2])
+    positions = random.sample({(x,y,z)   # random sample without replacement
+                               for x in range(dim[0])
+                               for y in range(dim[1])
+                               for z in range(dim[2])}, num_samples)
+    _start_time = time.time()
+    for p in positions:
+        si = ObjectState(1, "cube", p, res=1)
+        octree_belief[si] = random.uniform(0,1)*10
+    _total_time = time.time() - _start_time
+    print(f"Inserting {num_samples} leaves to octree belief took {_total_time:.4f}s)")
+
+    _start_time = time.time()
+    for i in range(sample_count):
+        s = octree_belief.random()
+    _total_time = time.time() - _start_time
+    num_leaves = len(octree_belief.octree.get_leaves())
+    print(f"Sampling {dim} octree belief (with {num_leaves} leaves) {sample_count} took {_total_time:.4f}s) ({_total_time/sample_count:.9f}s per sample)\n\n")
