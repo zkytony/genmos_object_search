acc1ad31a858481a0dc5dcdb869573ff1136f13c
Kaiyu Zheng <kaiyutony@gmail.com>
2022-08-13 18:39:19 -0400

    instruction for running test

--- ros/tests/spot/grpc/test_create_agent_with_map_from_point_cloud.py
+++ ros/tests/spot/grpc/test_create_agent_with_map_from_point_cloud.py
@@ -1,6 +1,8 @@
-# requires running
+# To run the test
 #
-# roslaunch rbd_spot_perception graphnav_map_publisher.launch map_name:=<map_name>
+# 1. In one terminal, run 'roslaunch rbd_spot_perception graphnav_map_publisher.launch map_name:=<map_name>'
+# 2. In another terminal, run 'python -m sloop_object_search.grpc.server'
+# 3. In another terminal, run this test 'python test_create_agent_with_map_from_point_cloud.py'
 #
 # Requires both point cloud and waypoints
 import numpy as np
