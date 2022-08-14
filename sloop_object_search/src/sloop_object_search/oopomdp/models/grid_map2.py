827efa16fa257b14dc72d92191a943c497fc0a99
Kaiyu Zheng <kaiyutony@gmail.com>
2022-08-13 21:42:43 -0400

    wrote basic functions for GridMap2d

--- sloop_object_search/src/sloop_object_search/oopomdp/models/grid_map2.py
+++ sloop_object_search/src/sloop_object_search/oopomdp/models/grid_map2.py
@@ -16,11 +16,14 @@
 # Unspecified grids will have "unknown" occupancy.
 #
 # Note that because the grids live on a lattice, the grid coordinates
-# are integers, and there will always be a (0,0) grid.
+# are integers, and there will always be a (0,0) grid. This forms the
+# coordinate system of the 2D object search agent.
 #
 # One benefit of not requiring non-negative coordinates is that the
 # grid map can be extended in any direction, while keeping the coordinates
 # of known grid cells unchanged.
+
+from sloop_object_search.utils.conversion import Frame, convert
 
 class GridMap2:
     def __init__(self, name="grid_map2",
@@ -38,3 +41,72 @@ class GridMap2:
                 each grid cell has length equal to 0.25m in the world frame.
             labels (dict): maps from location to a set of string labels
         """
+        if obstacles is None and free_locations is None:
+            raise ValueError("Need to supply obstacles or free_locations (or both)")
+
+        self.obstacles = set()
+        self.free_locations = set()
+        self.add_grids(obstacles=obstacles, free_locations=free_locations)
+
+        self.world_origin = world_origin
+        self.grid_size = grid_size
+
+        self.labels = labels
+        if labels is None:
+            self.labels = {}  # maps from position (x,y) to a set of labels
+
+    def add_grids(self, obstacles=None, free_locations=None):
+        if obstacles is not None:
+            self.obstacles |= obstacles
+        if free_locations is not None:
+            self.free_locations |= free_locations
+
+        # check that obstacles and free locations are disjoint
+        overlap = self.obstacles.intersection(self.free_locations)
+        if len(overlap) > 0:
+            raise ValueError("Error in argument: obstacles and free_locations are not disjoint. "\
+                             f"Overlap: {overlap}")
+
+    def all_grids(self):
+        return self.free_locations | self.obstacles
+
+    def is_unknown(self, loc):
+        return not (loc in self.free_location or loc in self.obstacles)
+
+    @property
+    def width(self):
+        all_grids = self.all_grids
+        min_x = min(all_grids, key=lambda g: g[0])[0]
+        max_x = max(all_grids, key=lambda g: g[0])[0]
+        return max_x - min_x
+
+    @property
+    def length(self):
+        all_grids = self.all_grids
+        min_y = min(all_grids, key=lambda g: g[1])[1]
+        max_y = max(all_grids, key=lambda g: g[1])[1]
+        return max_y - min_y
+
+    @property
+    def min_corner(self):
+        """The (x,y) location with the minimum x, and minimum y;
+        This is the corner grid, which, combined with 'width' and
+        'length', could be used for iterating over the lattice
+        covered by this grid map.
+        """
+        all_grids = self.all_grids
+        min_x = min(all_grids, key=lambda g: g[0])[0]
+        min_y = min(all_grids, key=lambda g: g[1])[1]
+        return (min_x, min_y)
+
+    def to_world_pos(self, x, y):
+        """converts a grid position to a world frame position"""
+        return convert((x, y), Frame.POMDP_SPACE, Frame.WORLD,
+                       region_origin=self.world_origin,
+                       search_space_resolution=self.grid_size)
+
+    def to_grid_pos(self, world_x, world_y):
+        """converts a grid position to a world frame position"""
+        return convert((world_x, world_y), Frame.WORLD, Frame.POMDP_SPACE,
+                       region_origin=self.world_origin,
+                       search_space_resolution=self.grid_size)
