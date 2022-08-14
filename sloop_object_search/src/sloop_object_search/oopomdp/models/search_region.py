6e7b5c205f8d185d91b3650a7bf628c86f81bfc2
Kaiyu Zheng <kaiyutony@gmail.com>
2022-08-14 18:14:09 -0400

    search_region minimal

--- sloop_object_search/src/sloop_object_search/oopomdp/models/search_region.py
+++ sloop_object_search/src/sloop_object_search/oopomdp/models/search_region.py
@@ -4,17 +4,17 @@ class SearchRegion:
         pass
 
 
-class SearchRegion2D:
+class SearchRegion2D(SearchRegion):
     """The 2D search region is represented as a GridMap"""
     def __init__(self, grid_map):
         """
         Args:
-            grid_map (GridMap):
+            grid_map (GridMap2):
         """
-        pass
+        self.grid_map = grid_map
 
 
-class SearchRegion3D:
+class SearchRegion3D(SearchRegion):
     """The 3D search region is represented as a collection
     of surfaces, using Open3D's datastructure; This is to
     support reasoning about occlusion while being efficient."""
