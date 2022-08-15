f29b535a72c1b344ad1ac9889d4bdd18ff151aa7
Kaiyu Zheng <kaiyutony@gmail.com>
2022-08-15 16:28:19 -0400

    more checks

--- sloop_object_search/src/sloop_object_search/grpc/utils/agent_utils.py
+++ sloop_object_search/src/sloop_object_search/grpc/utils/agent_utils.py
@@ -1,4 +1,10 @@
-from sloop_object_search.oopomdp.agent import AGENT_CLASS_2D, AGENT_CLASS_3D
+from sloop_object_search.oopomdp.agent import AGENT_CLASS_2D, AGENT_CLASS_3D,\
+    SloopMosBasic2DAgent, MosBasic2DAgent, SloopMosTopo2DAgent, MosBasic3DAgent
+
+VALID_AGENTS = {"SloopMosBasic2DAgent",
+                "MosBasic2DAgent",
+                "SloopMosTopo2DAgent",
+                "MosBasic3DAgent"}
 
 def create_agent(agent_name, agent_config_world, robot_pose, search_region):
     """
@@ -29,11 +35,16 @@ def create_agent(agent_name, agent_config_world, robot_pose, search_region):
 
     Note that by default, size units in agent_config that are metric.
     """
+    _validate_agent_config(agent_config_pomdp)
     agent_config_pomdp = _convert_metric_fields_to_pomdp_fields(agent_config_world)
-    _verify_agent_config(agent_config_pomdp)
 
 
-def _verify_agent_config(agent_config):
+
+def _validate_agent_config(agent_config):
+    if "agent_class" not in agent_config:
+        raise KeyError("'agent_class' must exist agent_config")
+    if "agent_class" not in VALID_AGENTS:
+        raise ValueError(f"agent class {agent_config['agent_config']} is invalid")
     if "robot" not in agent_config:
         raise KeyError("'robot' must exist agent_config")
     if "objects" not in agent_config:
@@ -56,7 +67,20 @@ def _verify_agent_config(agent_config):
     for objid in agent_config["robot"]["detectors"]:
         if objid not in agent_config["objects"]:
             raise ValueError(f"detectable object {objid} is not a defined object.")
-
+        # If there is 'params', and it is a list of two elements, change it
+        # to be a dict with 'sensor', 'quality'
+        if "params" in agent_config["robot"]["detectors"][objid]:
+            params = agent_config["robot"]["detectors"][objid]["params"]
+            if type(params) == list:
+                assert len(params) == 2,\
+                    "'params' for detector model should be [sensor_params, quality_params]"
+                params_dict = {"sensor": params[0], "quality": params[1]}
+                agent_config["robot"]["detectors"][objid]["params"] = params_dict
+            elif type(params) == dict:
+                assert "sensor" in params,\
+                    "'params' for detector model for {} doesn't have sensor params".format(objid)
+                assert "quality" in params,\
+                    "'params' for detector model for {} doesn't have quality params".format(objid)
 
 def _convert_metric_fields_to_pomdp_fields(agent_config_world):
     """POMDP agent takes in config in POMDP space. We do this conversion for detector specs."""
