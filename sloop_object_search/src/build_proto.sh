f34ad51b65206fa8c669273e960929788f795fce
Kaiyu Zheng <kaiyutony@gmail.com>
2022-08-15 05:18:53 -0400

    add build proto shell script

---
+++ sloop_object_search/src/build_proto.sh
@@ -0,0 +1,5 @@
+# run this script from this directory
+python -m grpc_tools.protoc -I ../protos/ --python_out=. --grpc_python_out=. ../protos/sloop_object_search/grpc/sloop_object_search.proto
+python -m grpc_tools.protoc -I ../protos/ --python_out=. --grpc_python_out=. ../protos/sloop_object_search/grpc/common.proto
+python -m grpc_tools.protoc -I ../protos/ --python_out=. --grpc_python_out=. ../protos/sloop_object_search/grpc/action.proto
+python -m grpc_tools.protoc -I ../protos/ --python_out=. --grpc_python_out=. ../protos/sloop_object_search/grpc/observation.proto
