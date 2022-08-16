ae5d3a62e3607ee4919e0af8015d4265b8fdd03b
Kaiyu Zheng <kaiyutony@gmail.com>
2022-08-16 16:40:21 -0400

    agent.py->common.py

--- sloop_object_search/src/sloop_object_search/oopomdp/agent/common.py
+++ sloop_object_search/src/sloop_object_search/oopomdp/agent/common.py
@@ -1,18 +1,38 @@
 import pomdp_py
+from sloop.agent import SloopAgent
 from sloop_object_search.utils.misc import import_class
 from ..models.search_region import SearchRegion2D
 from ..models.observation_model import (GMOSObservationModel,
                                         RobotObservationModel,
                                         IdentityLocalizationModel)
 from ..models.reward_model import GoalBasedRewardModel
+from ..models.transition_model import RobotTransTopo
 from . import belief
 
 
-class Mos2DAgent(pomdp_py.Agent):
-    """The top-level class for 2D agent. A 2D agent is
-    one who believes objects lie on a 2D plane, and it
-    carries a 2D sensor (e.g. fan-shaped sensor).
+def init_detection_models(agent_config):
+    robot = agent_config["robot"]
+    detection_models = {}
+    for objid in robot["detectors"]:
+        detector_spec = robot["detectors"][objid]
+        detection_model = import_class(detector_spec["class"])(
+            objid, *detector_spec["params"]
+        )
+        detection_models[objid] = detection_model
+    return detection_models
 
+def interpret_localization_model(robot_config):
+    """observation model of robot's own state"""
+    localization_model = robot_config.get("localization_model", "identity")
+    if localization_model == "identity":
+        return IdentityLocalizationModel()
+    else:
+        return import_class(localization_model)(
+            **robot_config.get("localization_model_args", {}))
+
+
+class MosAgent(pomdp_py.Agent):
+    """The top-level class for a mult-object search agent.
     The action space and transition model are not specified here."""
     def __init__(self, agent_config, search_region,
                  init_robot_belief,
@@ -24,8 +44,7 @@ class Mos2DAgent(pomdp_py.Agent):
             init_robot_belief (pomdp_py.GenerativeDistribution): belief over robot state
             init_object_beliefs (dict): maps from object id to pomdp_py.GenerativeDistribution
         """
-        assert isinstance(search_region, SearchRegion2D),\
-            "search region of a 2D agent should of type SearchRegion2D."
+
         self.agent_config = agent_config
         self.search_region = search_region
         robot = agent_config["robot"]
@@ -73,22 +92,20 @@ class Mos2DAgent(pomdp_py.Agent):
         raise NotImplementedError()
 
 
-def init_detection_models(agent_config):
-    robot = agent_config["robot"]
-    detection_models = {}
-    for objid in robot["detectors"]:
-        detector_spec = robot["detectors"][objid]
-        detection_model = import_class(detector_spec["class"])(
-            objid, *detector_spec["params"]
-        )
-        detection_models[objid] = detection_model
-    return detection_models
+class SloopMosAgent(SloopAgent):
+    def __init__(self, agent_config, search_region, init_robot_belief,
+                 init_object_beliefs=None):
+        if not isinstance(search_region, SearchRegion2D):
+            raise TypeError("SloopMosAgent requires 2D search region"
+                            "because spatial language currently works in 2D.")
+        map_name = search_region.grid_map.name
+        self.search_region = search_region
+        super().__init__(agent_config, map_name,
+                         init_robot_belief=init_robot_belief,
+                         init_object_beliefs=init_object_beliefs)
 
-def interpret_localization_model(robot_config):
-    """observation model of robot's own state"""
-    localization_model = robot_config.get("localization_model", "identity")
-    if localization_model == "identity":
-        return IdentityLocalizationModel()
-    else:
-        return import_class(localization_model)(
-            **robot_config.get("localization_model_args", {}))
+    def _init_oopomdp(self, init_robot_belief=None, init_object_beliefs=None):
+        raise NotImplementedError()
+
+        # if init_robot_belief is None:
+        #     raise ValueError("To instantiate MosAgent, initial robot belief is required.")
