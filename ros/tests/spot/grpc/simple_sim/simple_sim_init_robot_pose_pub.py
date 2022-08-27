8b8372134ef93314d60ea8ad2a68f0164cf0d367
Kaiyu Zheng <kaiyutony@gmail.com>
2022-08-27 16:55:49 -0400

    change test config

--- ros/tests/spot/grpc/simple_sim/simple_sim_init_robot_pose_pub.py
+++ ros/tests/spot/grpc/simple_sim/simple_sim_init_robot_pose_pub.py
@@ -2,16 +2,17 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 from sloop_mos_ros import ros_utils
+from sloop_object_search.utils.math import euler_to_quat
 
 def main():
     rospy.init_node("simple_sim_init_robot_pose_publisher")
     x = rospy.get_param("~x")
     y = rospy.get_param("~y")
     z = rospy.get_param("~z")
-    qx = rospy.get_param("~qx")
-    qy = rospy.get_param("~qy")
-    qz = rospy.get_param("~qz")
-    qw = rospy.get_param("~qw")
+    thx = rospy.get_param("~thx")
+    thy = rospy.get_param("~thy")
+    thz = rospy.get_param("~thz")
+    qx, qy, qz, qw = euler_to_quat(thx, thy, thz)
     world_frame = rospy.get_param("~world_frame")
 
     pose_pub = rospy.Publisher("/simple_sim_env/init_robot_pose",
