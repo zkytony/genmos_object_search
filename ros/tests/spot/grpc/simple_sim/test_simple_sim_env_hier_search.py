f1fdf2efc8d7f62bbf478600bebd8132a3db4544
Kaiyu Zheng <kaiyutony@gmail.com>
2022-09-11 13:49:17 -0400

    minor change in test instruction hier search

--- ros/tests/spot/grpc/simple_sim/test_simple_sim_env_hier_search.py
+++ ros/tests/spot/grpc/simple_sim/test_simple_sim_env_hier_search.py
@@ -7,7 +7,7 @@
 # To run the test, do the following IN ORDER:
 # 0. run config_simple_sim_lab121_lidar.py to generate the .yaml configuration file
 # 1. run in a terminal 'roslaunch rbd_spot_perception graphnav_map_publisher.launch map_name:=lab121_lidar'
-# 2. run in a terminal 'roslaunch sloop_object_search_ros spot_local_cloud_publisher.launch robot_pose_topic:=/simple_sim_env/init_robot_pose'
+# 2. run in a terminal 'roslaunch sloop_object_search_ros spot_local_cloud_publisher.launch robot_pose_topic:=/simple_sim_env/robot_pose'
 # 3. run in a terminal 'roslaunch sloop_object_search_ros simple_sim_env.launch map_name:=lab121_lidar'
 # 4. run in a terminal 'python -m sloop_object_search.grpc.server'
 # 5. run in a terminal 'python test_simple_sim_env_hier_search.py'
