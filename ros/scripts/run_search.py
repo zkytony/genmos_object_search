559973f94d784b3f390b40ebb4a64e07b063ab26
Kaiyu Zheng <kaiyutony@gmail.com>
2022-09-11 15:13:07 -0400

    porting over code to update search region

--- ros/scripts/run_search.py
+++ ros/scripts/run_search.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 #
 # To run a test:
-# 1. run in one terminal, "roslaunch sloop_object_search_ros sloop_mos_spot_exp1_local_lab121.launch"
+# 1. run in a terminal 'python -m sloop_object_search.grpc.server'
+# 2. run in a terminal, "roslaunch sloop_object_search_ros sloop_mos_spot_exp1_local_lab121.launch"
 #    (replace 'sloop_mos_spot_exp1_local_lab121.launch' with the experiment launch file you want)
-# 2. run rviz
+# 3. run rviz
 from sloop_mos_ros import SloopMosROS
 
 def main():
--- ros/src/sloop_mos_ros/sloop_mos.py
+++ ros/src/sloop_mos_ros/sloop_mos.py
@@ -17,6 +17,7 @@ from sloop_object_search.grpc import observation_pb2 as o_pb2
 from sloop_object_search.grpc import action_pb2 as a_pb2
 from sloop_object_search.grpc import common_pb2
 from sloop_object_search.grpc.common_pb2 import Status
+from sloop_object_search.grpc.constants import Message
 from sloop_object_search.utils.colors import lighter
 from sloop_object_search.utils import math as math_utils
 
@@ -28,6 +29,93 @@ class SloopMosROS:
     def __init__(self, name="sloop_ros"):
         self.name = name
         self._sloop_client = None
+
+    def server_message_callback(self, message):
+        if Message.match(message) == Message.REQUEST_LOCAL_SEARCH_REGION_UPDATE:
+            local_robot_id = Message.forwhom(message)
+            rospy.loginfo(f"will send a update search request to {local_robot_id}")
+            self.update_search_region_3d(robot_id=local_robot_id)
+            self._local_robot_id = local_robot_id
+        elif Message.match(message) == Message.LOCAL_AGENT_REMOVED:
+            local_robot_id = Message.forwhom(message)
+            rospy.loginfo(f"local agent {local_robot_id} removed.")
+            if local_robot_id != self._local_robot_id:
+                rospy.logerr("removed local agent has an unexpected ID")
+            self._local_robot_id = None
+
+    def update_search_region_2d(self, robot_id=None):
+        # need to get a region point cloud and a pose use that as search region
+        if robot_id is None:
+            robot_id = self.robot_id
+        rospy.loginfo("Sending request to update search region (2D)")
+
+        region_cloud_msg, pose_stamped_msg = ros_utils.WaitForMessages(
+            [self._search_region_2d_topic, self._robot_pose_topic],
+            [sensor_msgs.PointCloud2, geometry_msgs.PoseStamped],
+            delay=10000, verbose=True).messages
+
+        cloud_pb = ros_utils.pointcloud2_to_pointcloudproto(region_cloud_msg)
+        robot_pose = ros_utils.pose_to_tuple(pose_stamped_msg.pose)
+        robot_pose_pb = proto_utils.robot_pose_proto_from_tuple(robot_pose)
+
+        search_region_config = self.agent_config.get("search_region", {}).get("2d", {})
+        search_region_params_2d = dict(
+            layout_cut=search_region_config.get("layout_cut", 0.6),
+            grid_size=search_region_config.get("res", SEARCH_SPACE_RESOLUTION_2D),
+            brush_size=search_region_config.get("brush_size", 0.5),
+            debug=search_region_config.get("debug", False)
+        )
+        self._sloop_client.updateSearchRegion(
+            header=cloud_pb.header,
+            robot_id=robot_id,
+            robot_pose=robot_pose_pb,
+            point_cloud=cloud_pb,
+            search_region_params_2d=search_region_params_2d)
+
+    def update_search_region_3d(self, robot_id=None):
+        if robot_id is None:
+            robot_id = self.robot_id
+        rospy.loginfo("Sending request to update search region (3D)")
+        region_cloud_msg, pose_stamped_msg = ros_utils.WaitForMessages(
+            [self._search_region_3d_topic, self._robot_pose_topic],
+            [sensor_msgs.PointCloud2, geometry_msgs.PoseStamped],
+            delay=100, verbose=True).messages
+        cloud_pb = ros_utils.pointcloud2_to_pointcloudproto(region_cloud_msg)
+        robot_pose = ros_utils.pose_to_tuple(pose_stamped_msg.pose)
+        robot_pose_pb = proto_utils.robot_pose_proto_from_tuple(robot_pose)
+
+        # parameters
+        search_region_config = self.agent_config.get("search_region", {}).get("3d", {})
+        search_region_params_3d = dict(
+            octree_size=search_region_config.get("octree_size", 32),
+            search_space_resolution=search_region_config.get("res", SEARCH_SPACE_RESOLUTION_3D),
+            region_size_x=search_region_config.get("region_size_x", 4.0),
+            region_size_y=search_region_config.get("region_size_y", 4.0),
+            region_size_z=search_region_config.get("region_size_z", 2.0),
+            debug=search_region_config.get("debug", False)
+        )
+        self._sloop_client.updateSearchRegion(
+            header=cloud_pb.header,
+            robot_id=robot_id,
+            robot_pose=robot_pose_pb,
+            point_cloud=cloud_pb,
+            search_region_params_3d=search_region_params_3d)
+
+    def update_search_region(self):
+        if self.agent_config["agent_type"] == "local":
+            if self.agent_config["agent_class"].endswith("3D"):
+                self.update_search_region_3d()
+            else:
+                self.update_search_region_2d()
+        elif self.agent_config["agent_type"] == "hierarchical":
+            # local agent in hierarchical planning will get its search region
+            # through server-client communication. Here, the client only needs
+            # to send over the search region info for the global agent.
+            self.update_search_region_2d()
+        else:
+            raise ValueError("Unexpected agent type: {}"\
+                             .format(self.agent_config["agent_type"]))
+
 
     def main(self):
                  # o3dviz=False, prior="uniform",
@@ -62,12 +150,14 @@ class SloopMosROS:
         self._belief_2d_markers_pub = rospy.Publisher(
             "~belief_2d", MarkerArray, queue_size=10, latch=True)
 
-        self.search_space_res_3d = rospy.get_param("search_res_3d", SEARCH_SPACE_RESOLUTION_3D)
-        self.search_space_res_2d = rospy.get_param("search_res_2d", SEARCH_SPACE_RESOLUTION_2D)
-
-
-
         # First, create an agent
         self._sloop_client.createAgent(
             header=proto_utils.make_header(), config=self.agent_config,
             robot_id=self.robot_id)
+
+        # Make the client listen to server
+        ls_future = self._sloop_client.listenToServer(
+            self.robot_id, self.server_message_callback)
+        self._local_robot_id = None  # needed if the planner is hierarchical
+
+        # Update search region
