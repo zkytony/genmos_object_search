21f02a0c8b7075d923f0d581ba794c0e4341c4de
Kaiyu Zheng <kaiyutony@gmail.com>
2022-09-13 00:45:17 -0400

    refactor

--- sloop_object_search/src/sloop_object_search/grpc/utils/search_region_processing.py
+++ sloop_object_search/src/sloop_object_search/grpc/utils/search_region_processing.py
@@ -9,6 +9,7 @@ from sloop_object_search.utils.math import (remap, euclidean_dist,
                                             eucdist_multi, in_square, in_square_multi)
 from sloop_object_search.utils.conversion import Frame, convert
 from sloop_object_search.utils import open3d_utils
+from sloop_object_search.utils.algo import flood_fill_2d
 from sloop_object_search.oopomdp.models.grid_map import GridMap
 from sloop_object_search.oopomdp.models.grid_map2 import GridMap2
 from sloop_object_search.oopomdp.models.search_region import SearchRegion2D, SearchRegion3D
@@ -121,56 +122,6 @@ def search_region_3d_from_point_cloud(point_cloud, robot_position, existing_sear
 
 
 ########### Auxiliary functions for 2D Search Regeion ##############
-def flood_fill_2d(grid_points, seed_point, grid_brush_size=2, flood_region_size=None):
-    """
-    Given a numpy array of points that are supposed to be on a grid map,
-    and a "seed point", flood fill by adding more grid points that are
-    in empty locations to the set of grid points, starting from the seed
-    point. Will not modify 'grid_points' but returns a new array.
-
-    Args:
-        grid_points (np.ndarray): a (n,2) or (n,3) array
-        seed_point (np.ndarray or tuple): dimension should match that of a grid point
-        grid_brush_size (int): The length (number of grids) of a square brush
-            which will be used to fill out the empty spaces.
-        flood_region_size (float): the maximum size (number of grids) of
-            the flooding region which is a square.
-    """
-    def _neighbors(p, d=1):
-        # this works with both 2D or 3D points
-        return set((p[0] + dx, p[1] + dy, *p[2:])
-                   for dx in range(-d, d+1)
-                   for dy in range(-d, d+1)
-                   if not (dx == 0 and dy == 0))
-
-    seed_point = tuple(seed_point)
-    if grid_points.shape[1] != len(seed_point):
-        raise ValueError("grid points and seed point have different dimensions.")
-
-    grid_points_set = set(map(tuple, grid_points))
-    # BFS
-    worklist = deque([seed_point])
-    visited = set({seed_point})
-    new_points = set()
-    while len(worklist) > 0:
-        point = worklist.popleft()
-        # Imagine placing a square brush centered at the point.
-        # We assume that the point always represents a valid free cell
-        brush_points = _neighbors(point, d=max(1, grid_brush_size//2))
-        new_points.update(brush_points)
-        if not any(bp in grid_points_set for bp in brush_points):
-            # This brush stroke fits; we will consider all brush points
-            # as potential free cells - i.e. neighbors
-            for neighbor_point in brush_points:
-                if neighbor_point not in visited:
-                    if flood_region_size is not None:
-                        if not in_square(neighbor_point, seed_point, flood_region_size):
-                            continue  # skip this point: too far.
-                    worklist.append(neighbor_point)
-                    visited.add(neighbor_point)
-    return np.array(list(grid_points_set | new_points))
-
-
 def points_to_search_region_2d(points, robot_position, existing_search_region=None, **kwargs):
     """
     Given a Numpy array of points (N,3), the robot current pose, and
