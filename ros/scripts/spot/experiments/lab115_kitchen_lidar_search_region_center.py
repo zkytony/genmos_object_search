a0fc3201328d76d6f7a085d51b8449d1ddf6e73d
Kaiyu Zheng <kaiyutony@gmail.com>
2022-09-14 17:13:07 -0400

    other files for new env

---
+++ ros/scripts/spot/experiments/lab115_kitchen_lidar_search_region_center.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import PoseStamped
+from sloop_mos_ros import ros_utils
+from sloop_object_search.utils.math import euler_to_quat
+
+def main():
+    rospy.init_node("local_search_center_publisher")
+    x = -0.17396485805511475
+    y = -0.1354684829711914
+    z = 0.25
+    qx = 0.0
+    qy = 0.0
+    qz = 0.9992697136539744
+    qw = 0.038210461576692244
+    world_frame = "graphnav_map"
+    pose_pub = rospy.Publisher("/local_region_center", PoseStamped, queue_size=10, latch=True)
+    rate = rospy.Rate(5)
+    print("Publishing local region center")
+    while not rospy.is_shutdown():
+        msg = ros_utils.pose_tuple_to_pose_stamped(
+            (x,y,z,qx,qy,qz,qw), world_frame)
+        pose_pub.publish(msg)
+        rate.sleep()
+
+if __name__ == "__main__":
+    main()
