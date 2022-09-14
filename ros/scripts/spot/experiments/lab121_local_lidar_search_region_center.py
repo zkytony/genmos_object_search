2a3f12d41ded2c57b52242f843ae2f30706af0c2
Kaiyu Zheng <kaiyutony@gmail.com>
2022-09-13 23:30:33 -0400

    lower rate

--- ros/scripts/spot/experiments/lab121_local_lidar_search_region_center.py
+++ ros/scripts/spot/experiments/lab121_local_lidar_search_region_center.py
@@ -15,7 +15,7 @@ def main():
     qw = 0.038210461576692244
     world_frame = "graphnav_map"
     pose_pub = rospy.Publisher("/local_region_center", PoseStamped, queue_size=10, latch=True)
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(5)
     print("Publishing local region center")
     while not rospy.is_shutdown():
         msg = ros_utils.pose_tuple_to_pose_stamped(
