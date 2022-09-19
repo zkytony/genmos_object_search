ecf77efcfe9ca7a9501d368f5b85d1842f161412
Kaiyu Zheng <kaiyutony@gmail.com>
2022-09-19 18:02:49 -0400

    sh exp script

---
+++ ros/tests/spot/grpc/simple_sim/exp_sim_env_local_search_3d.sh
@@ -0,0 +1,5 @@
+python test_simple_sim_env_local_search_3d.py groundtruth 32 config_simple_sim_lab121_lidar.yaml
+python test_simple_sim_env_local_search_3d.py uniform 32 config_simple_sim_lab121_lidar.yaml
+python test_simple_sim_env_local_search_3d.py occupancy 32 config_simple_sim_lab121_lidar.yaml
+python test_simple_sim_env_local_search_3d.py uniform 16 config_simple_sim_lab121_lidar.yaml
+python test_simple_sim_env_local_search_3d.py occupancy 16 config_simple_sim_lab121_lidar.yaml
