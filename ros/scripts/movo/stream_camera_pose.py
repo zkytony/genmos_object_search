4a4542436f00eb99f5f99a3a89b749a6f3a20c16
Kaiyu Zheng <kaiyutony@gmail.com>
2022-09-22 03:14:48 -0400

    movo system starts in noetic

---
+++ ros/scripts/movo/stream_camera_pose.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import rospy
+import tf2_ros
+import geometry_msgs, std_msgs
+from sloop_mos_ros.ros_utils import transform_to_pose_stamped
+
+def main():
+    rospy.init_node("movo_stream_camera_pose")
+
+    tfBuffer = tf2_ros.Buffer()
+    listener = tf2_ros.TransformListener(tfBuffer)
+    pose_pub = rospy.Publisher("/movo_camera_pose", geometry_msgs.msg.PoseStamped, queue_size=10)
+
+    rate = rospy.Rate(10)
+    last_ex = None
+    message_printed = False
+    while not rospy.is_shutdown():
+        try:
+            timestamp = rospy.Time(0)
+            trans_stamped = tfBuffer.lookup_transform("map", "kinect2_color_frame", timestamp)
+            pose_msg = transform_to_pose_stamped(trans_stamped.transform,
+                                                 "map",
+                                                 stamp=trans_stamped.header.stamp)
+            pose_pub.publish(pose_msg)
+            if not message_printed:
+                rospy.loginfo("publishing camera pose")
+                message_printed = True
+
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as ex:
+            rate.sleep()
+            if last_ex is None or str(last_ex) != str(ex):
+                rospy.logerr(ex)
+            last_ex = ex
+            continue
+
+if __name__ == "__main__":
+    main()
--- ros/scripts/run_search.py
+++ ros/scripts/run_search.py
@@ -7,6 +7,13 @@
 # 4. run in a terminal, "roslaunch sloop_object_search_ros sloop_mos_spot_exp1_local_lab121.launch"
 #    (replace 'sloop_mos_spot_exp1_local_lab121.launch' with the experiment launch file you want)
 # 5. run rviz, 'roslaunch sloop_object_search_ros view_spot_local_search.launch'
+###########################################################################
+# To run a test (For MOVO):
+# 1. bootup movo. Then start navigation stack on movo2: roslaunch movo_demos map_nav.launch map_file:=scili8_living_room
+# 2. run in a terminal 'roslaunch sloop_object_search_ros sloop_mos_movo_system.launch map_name:=<map_name>'
+# 3. run in a terminal 'python -m sloop_object_search.grpc.server'
+# 4. run in a terminal, "roslaunch sloop_object_search_ros sloop_mos_movo_exp1_scili8_living_room.launch"
+# 5. run rviz, 'roslaunch sloop_object_search_ros view_movo_object_search.launch'
 from sloop_mos_ros import SloopMosROS
 
 def main():
