928753357780418f3eed81c267ca07752932a574
Kaiyu Zheng <kaiyutony@gmail.com>
2022-09-22 12:29:33 -0400

    progress on executor & configuring topics

--- ros/src/sloop_mos_movo/action_executor.py
+++ ros/src/sloop_mos_movo/action_executor.py
@@ -5,6 +5,9 @@ import geometry_msgs.msg as geometry_msgs
 
 import sys
 import diagnostic_msgs
+import std_msgs.msg as std_msgs
+from actionlib_msgs.msg import GoalStatus
+
 from pomdp_py.utils import typ
 
 from sloop_object_search_ros.msg import KeyValAction
@@ -16,8 +19,76 @@ from sloop_mos_ros import ros_utils
 class MovoSloopActionExecutor(ActionExecutor):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self._robot_pose_topic = "~robot_pose"
+        self.robot_id = rospy.get_param("~robot_id")
+        self.world_frame = rospy.get_param("~world_frame")
+        self.camera_frame = "kinect2_color_frame"
+        self.body_frame = "base_link"
+        self.check_before_execute = rospy.get_param("~check_before_execute", True)
         self._goal_viz_pub = rospy.Publisher(
             "~goal_markers", MarkerArray, queue_size=10, latch=True)
 
     def _execute_action_cb(self, msg):
-        import pdb; pdb.set_trace()
+        if msg.type == "nothing":
+            return
+
+        kv = {msg.keys[i]: msg.values[i] for i in range(len(msg.keys))}
+        # used to identify this action as a goal for execution
+        action_id = kv["action_id"]
+        rospy.loginfo("received action to execute")
+
+        if msg.type == "nav":
+            goal_x = float(kv["goal_x"])
+            goal_y = float(kv["goal_y"])
+            goal_z = float(kv["goal_z"])
+            goal_qx = float(kv["goal_qx"])
+            goal_qy = float(kv["goal_qy"])
+            goal_qz = float(kv["goal_qz"])
+            goal_qw = float(kv["goal_qw"])
+            nav_type = kv["nav_type"]
+            self.publish_status(GoalStatus.ACTIVE,
+                                typ.info(f"executing {kv['action_id']}..."),
+                                action_id, msg.stamp)
+            success = self.move_viewpoint(action_id, nav_type,
+                                          (goal_x, goal_y, goal_z,
+                                           goal_qx, goal_qy, goal_qz, goal_qw))
+            if success:
+                self.publish_status(GoalStatus.SUCCEEDED,
+                                    typ.success("move viewpoint succeeded"),
+                                    action_id, msg.stamp, pub_done=True)
+            else:
+                self.publish_status(GoalStatus.ABORTED,
+                                    typ.error("move viewpoint failed"),
+                                    action_id, msg.stamp, pub_done=True)
+
+        elif msg.type == "find":
+            # Move backwards a little bit
+            vx = 0.4
+            # command_id, _ = rbd_spot.body.velocityCommand(
+            # self.conn, self.command_client, -vx, 0.0, 0.0, duration=0.2/vx)
+            # # signal find action by closing and opening gripper
+            # rbd_spot.arm.close_gripper(self.conn, self.command_client)
+            # rbd_spot.arm.open_gripper(self.conn, self.command_client)
+            self.publish_status(GoalStatus.SUCCEEDED,
+                                typ.success("find action succeeded"),
+                                action_id, msg.stamp, pub_done=True)
+
+
+    def move_viewpoint(self, action_id, nav_type, goal_pose):
+        if nav_type not in {"2d", "3d"}:
+            raise ValueError("nav_type should be '2d' or '3d'")
+
+        # Visualize the goal.
+        # clear markers first
+        clear_msg = ros_utils.clear_markers(std_msgs.Header(stamp=rospy.Time.now(),
+                                                            frame_id=self.world_frame), ns="")
+        self._goal_viz_pub.publish(clear_msg)
+
+        # then make the markers for goals; We will still visualize hand messages (that's what we care about)
+        _marker_scale = geometry_msgs.Vector3(x=0.4, y=0.05, z=0.05)
+        _goal_marker_msg = ros_utils.make_viz_marker_from_robot_pose_3d(
+            self.robot_id + "_goal", goal_pose, std_msgs.Header(frame_id=self.world_frame, stamp=rospy.Time.now()),
+            scale=_marker_scale, color=[0.53, 0.95, 0.99, 0.9], lifetime=0)
+        self._goal_viz_pub.publish(MarkerArray([_goal_marker_msg]))
+
+        return False
