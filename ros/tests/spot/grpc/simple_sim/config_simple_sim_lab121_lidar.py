8e2e6e083463a815e5d6eb4f0651410891c82fcd
Kaiyu Zheng <kaiyutony@gmail.com>
2022-09-29 10:46:55 -0400

    actually, occlusion should be fair game.

--- ros/tests/spot/grpc/simple_sim/config_simple_sim_lab121_lidar.py
+++ ros/tests/spot/grpc/simple_sim/config_simple_sim_lab121_lidar.py
@@ -96,7 +96,7 @@ CONFIG_LOCAL = {
                 'params': {'fov': 60,
                            'far': 2.50,
                            'near': 0.2,
-                           'occlusion_enabled': False}
+                           'occlusion_enabled': True}
             }],
             'detectors': {'book': {'class': 'sloop_object_search.oopomdp.FrustumVoxelAlphaBeta',
                                    'params': {"sensor": "camera",
