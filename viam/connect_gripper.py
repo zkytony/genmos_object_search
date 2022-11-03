861a58f5ee35b82df77efcfc7a7679960245ccee
Kaiyu Zheng <kaiyutony@gmail.com>
2022-11-03 13:22:54 -0400

    script to connect to gripper directly

---
+++ viam/connect_gripper.py
@@ -0,0 +1,25 @@
+import asyncio
+
+from viam.robot.client import RobotClient
+from viam.rpc.dial import Credentials, DialOptions
+
+async def connect():
+    creds = Credentials(
+        type='robot-location-secret',
+        payload='gm1rjqe84nt8p64ln6r1jyf5hc3tdnc2jywojoykvk56d0qa')
+    opts = RobotClient.Options(
+        refresh_interval=0,
+        dial_options=DialOptions(credentials=creds)
+    )
+    return await RobotClient.at_address('gripper-main.tcyat99x8y.viam.cloud', opts)
+
+async def main():
+    robot = await connect()
+
+    print('Resources:')
+    print(robot.resource_names)
+
+    await robot.close()
+
+if __name__ == '__main__':
+    asyncio.run(main())
