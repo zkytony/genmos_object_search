27ba641f87076d405f83c5c4d0386509210522d3
Kaiyu Zheng <kaiyutony@gmail.com>
2022-11-03 13:23:20 -0400

    config file for ur5 test trial

---
+++ viam/config/ur5_exp1_viamlab.py
@@ -0,0 +1,120 @@
+######### COMMON CONFIGURATIONS ######
+LOCAL_BELIEF = {"visible_volume_params": {"num_rays": 150,
+                                          "step_size": 0.2,
+                                          "voxel_res": 2},
+                "init_params": {"num_samples": 3000,
+                                "prior_from_occupancy": True,
+                                "occupancy_height_thres": 0.2,
+                                "occupancy_blow_up_res": 4,
+                                "occupancy_fill_height": True}}
+
+SEARCH_REGION_3D = {"res": 0.1,
+                    "octree_size": 32,
+                    "region_size_x": 3.0,
+                    "region_size_y": 3.0,
+                    "region_size_z": 1.5}
+
+# Official spot spec: RGB: 60.2deg x 46.4deg; Depth: 55.9deg x 44deg
+HAND_CAMERA = {'name': 'hand_camera',
+               'params': {'fov': 53,
+                          'far': 1.5,
+                          'near': 0.2,
+                          'aspect_ratio': 0.7,
+                          'occlusion_enabled': True}}
+
+HAND_FAN = {"name": 'hand_fan',
+            'params': {'fov': 50,
+                       'min_range': 0.2,
+                       'max_range': 1.5}}
+
+LOCAL_TOPO = {'num_nodes': 10,
+              'pos_importance_thres': 0.01,
+              'sep': 0.75,
+              'debug': False,
+              'resample_thres': 0.4,
+              '3d_proj_2d': {'layout_cut': 0.4,
+                             'floor_cut': 0.15,
+                             'brush_size': 0.2,
+                             'inflation': 0.5}}
+
+LOCAL_ACTION = {'topo': LOCAL_TOPO,
+                'policy': {'cost_scaling_factor': 1.0}}
+
+LOCAL_REACHABLE = {"min_height": 0.5,
+                   "max_height": 1.2}
+
+LOCAL_PLANNER_CONFIG = {"planner": "pomdp_py.POUCT",
+                        "planner_params": {
+                            "exploration_const": 1000,
+                            "max_depth": 6,
+                            "num_sims": 400,
+                            "show_progress": True}}
+
+######### DETECTORS AND OBJECTS ######
+LOCAL_DETECTORS = {
+    'Cat': {'class': 'sloop_object_search.oopomdp.FrustumVoxelAlphaBeta',
+            'params': {"sensor": "hand_camera",
+                       "quality": [1e7, 0.2]}},
+}
+
+OBJECTS = {
+    'Cat': {'class': 'Cat',
+            'transition': {'class': 'sloop_object_search.oopomdp.StaticObjectTransitionModel'},
+            'color': [0.67, 0.61, 0.15, 0.8],
+            'viz_type': 'cube'}
+}
+
+def make_objects(*objects):
+    result = {}
+    for obj in objects:
+        result[obj] = OBJECTS[obj]
+    return result
+
+def make_detectors(local_or_global, *objects):
+    assert local_or_global in {"local", "global"}
+    result = {}
+    for obj in objects:
+        if local_or_global == "local":
+            result[obj] = LOCAL_DETECTORS[obj]
+        else:
+            result[obj] = GLOBAL_DETECTORS[obj]
+    return result
+
+
+######### THE FOLLOWING IS USED FOR LOCAL SEARCH TEST #########
+target_objects = ["Cat", "ToyPlane", "Lyzol", "Pringles"]
+CONFIG_LOCAL = {
+    "agent_config": {
+        "agent_class": "MosAgentTopo3D",
+        "agent_type": "local",
+        'belief': LOCAL_BELIEF,
+        "search_region": {"3d": SEARCH_REGION_3D},
+        'robot': {
+            'id': 'robot0',
+            'no_look': True,
+            'sensors': [HAND_CAMERA],
+            'detectors': make_detectors("local", *target_objects),
+            'action': LOCAL_ACTION,
+            "reachable": LOCAL_REACHABLE,
+        },
+        'objects': make_objects("Cat", *target_objects),
+        'targets': target_objects
+    },
+
+    "task_config": {
+        "max_steps": 200
+    },
+
+    "planner_config": LOCAL_PLANNER_CONFIG
+}
+
+
+CONFIG = CONFIG_LOCAL
+
+import yaml
+def main():
+    with open("ur5_exp1_viamlab.yaml", "w") as f:
+        yaml.dump(CONFIG, f, default_flow_style=False)
+
+if __name__ == "__main__":
+    main()
