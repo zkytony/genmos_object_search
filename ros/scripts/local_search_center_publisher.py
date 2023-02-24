a4cfa8ba221513de0354959d4b781741bab4e53c
zkytony <kaiyutony@gmail.com>
2023-02-24 15:55:24 -0500

    minor center pose config ros param loading

--- ros/scripts/local_search_center_publisher.py
+++ ros/scripts/local_search_center_publisher.py
@@ -1,5 +1,3 @@
-## TODO: For local search, publish search region center too.
-
 #!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import PoseStamped
@@ -14,18 +12,17 @@ def main():
         rospy.get_param("{}/center_x".format(search_region_3d_config_prefix)),
         rospy.get_param("{}/center_y".format(search_region_3d_config_prefix)),
         rospy.get_param("{}/center_z".format(search_region_3d_config_prefix)),
-        rospy.get_param("{}/center_qx".format(search_region_3d_config_prefix)),
-        rospy.get_param("{}/center_qy".format(search_region_3d_config_prefix)),
-        rospy.get_param("{}/center_qz".format(search_region_3d_config_prefix)),
-        rospy.get_param("{}/center_qw".format(search_region_3d_config_prefix))
+        rospy.get_param("{}/center_qx".format(search_region_3d_config_prefix), 0.0),
+        rospy.get_param("{}/center_qy".format(search_region_3d_config_prefix), 0.0),
+        rospy.get_param("{}/center_qz".format(search_region_3d_config_prefix), 0.0),
+        rospy.get_param("{}/center_qw".format(search_region_3d_config_prefix), 1.0)
     )
     world_frame = rospy.get_param("~world_frame")
     pose_pub = rospy.Publisher("/local_region_center", PoseStamped, queue_size=10, latch=True)
     rate = rospy.Rate(5)
-    print("Publishing local region center")
+    rospy.loginfo(f"Publishing local region center: {center_pose}")
     while not rospy.is_shutdown():
-        msg = ros_utils.pose_tuple_to_pose_stamped(
-            (x,y,z,qx,qy,qz,qw), world_frame)
+        msg = ros_utils.pose_tuple_to_pose_stamped(center_pose, world_frame)
         pose_pub.publish(msg)
         rate.sleep()
 
