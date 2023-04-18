d00ed0af7a10087dd7aa4ac7d01f39ceeff949f7
Kaiyu Zheng <kzheng@theaiinstitute.com>
2023-04-18 17:55:56 -0400

    finally - made rviz config survive after colcon (re)build

--- ros2/tests/simple_sim/view_simple_sim_ros2.sh
+++ ros2/tests/simple_sim/view_simple_sim_ros2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-rviz2 -d $SCRIPTPATH/simple_sim_ros2.rv
+ABS_PKG_PATH=$BDAI/projects/search/ws/src/genmos_object_search_ros2
+rviz2 -d $ABS_PKG_PATH/tests/simple_sim/simple_sim_ros2.rv
