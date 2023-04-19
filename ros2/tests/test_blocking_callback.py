86212976022260b23ba717cdb0f8ebf1e65d5db3
Kaiyu Zheng <kzheng@theaiinstitute.com>
2023-04-18 23:10:04 -0400

    add an action publisher into the test

--- ros2/tests/test_blocking_callback.py
+++ ros2/tests/test_blocking_callback.py
@@ -6,6 +6,9 @@
 # 2. subscribes to a topic 'action'; each received action message causes the
 # state to change, a process that takes several seconds to complete.
 #
+# 3. publishes to a topic 'action_done' when the action execution has finished.
+#    (does not execute an action if a previous action was executing already).
+#
 # Question: How could we implement this node such that the action-change process
 # does not block state publishing?
 # /author: Kaiyu Zheng
@@ -27,9 +30,16 @@ class NodeSol(Node):
         self._state.point.y = 0.0
         self._state.point.z = 0.0
         self.state_pub = self.create_publisher(
-            geometry_msgs.msg.PointStamped, "state",
+            geometry_msgs.msg.PointStamped, "~/state",
+            QoSProfile(depth=10, durability=QoSDurabilityPolicy.TRANSIENT_LOCAL))
+        self.action_sub = self.create_subscription(
+            std_msgs.msg.String, "~/action", self.action_cb,
+            QoSProfile(depth=10, durability=QoSDurabilityPolicy.TRANSIENT_LOCAL))
+        self.action_done_pub = self.create_publisher(
+            std_msgs.msg.String, "~/action_done",
             QoSProfile(depth=10, durability=QoSDurabilityPolicy.TRANSIENT_LOCAL))
         self.timer = self.create_timer(0.3, self.publish_state)
+        self._executing = False
 
     def publish_state(self):
         self._state.header.stamp = self.get_clock().now().to_msg()
@@ -39,11 +49,36 @@ class NodeSol(Node):
                                        self._state.point.y,
                                        self._state.point.z))
 
+    def action_cb(self, msg):
+        if self._executing:
+            self.get_logger().info("action ignored - another action is executing")
+        else:
+            self.get_logger().info("action received!")
+            self.action_done_pub.publish(std_msgs.msg.String(data="done"))
+            self._executing = True
+
+
+class NodeActionPub(Node):
+    """a node used to publish actions for testing;
+    """
+    def __init__(self):
+        super().__init__("node_action_pub")
+        self.action_pub = self.create_publisher(
+            std_msgs.msg.String, "/node_sol/action",
+            QoSProfile(depth=10, durability=QoSDurabilityPolicy.TRANSIENT_LOCAL))
+        self.timer = self.create_timer(0.7, self.publish_action)
+
+    def publish_action(self):
+        self.action_pub.publish(std_msgs.msg.String(data="+x"))
+
+
 def test():
     rclpy.init()
     node_sol = NodeSol()
+    node_action_pub = NodeActionPub()
     executor = rclpy.executors.MultiThreadedExecutor(3)
     executor.add_node(node_sol)
+    executor.add_node(node_action_pub)
     executor.spin()
 
     node_foo.destroy_node()
